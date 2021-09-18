bcb27a567e64dbeb6c0e21d5e84acb1772d94b10
Ahmed Samir <39447006+ahmed-samir-mohammed@users.noreply.github.com>
2021-09-18 23:29:57 +0200

    Deploy

--- src/app/components/shared-gide/shared-gide.component.ts
+++ src/app/components/shared-gide/shared-gide.component.ts
@@ -9,12 +9,37 @@ import { GuideService } from 'src/app/core/services/guide.service';
 })
 export class SharedGideComponent implements OnInit {
 
-  guideListItems: GuideList[] = []
+  guideListItems: GuideList[] = [
+    {
+      id: 1,
+      name: "طارق بن عبد الله النعيم",
+      job: "الرئيس التنفيذي لشركة نقل وتقنيات المياه",
+      imgName: "img_1.jpeg"
+    },
+    {
+      id: 2,
+      name: "عبدالعزيز بن سالم الزيد",
+      job: "الرئيس التنفيذي لشركة خدمات الملاحة الجوية السعودية",
+      imgName: "img_2.jpeg"
+    },
+    {
+      id: 3,
+      name: "تركي عدنان عبد المجيد",
+      job: "الرئيس التنفيذي لقطاع الأغذية في مجموعة المداح التجارية",
+      imgName: "img_3.jpeg"
+    },
+    {
+      id: 4,
+      name: "عبدالله بن ناصر البدر",
+      job: "الرئيس التنفيذي لشركة المراعي",
+      imgName: "img_4.jpg"
+    }
+  ]
 
   constructor(private guideList: GuideService) { }
 
   ngOnInit(): void {
-    this.getGuideList()
+    // this.getGuideList()
   }
 
   // Get All News List
