e7d201c96b22c2b608975343ee6e457cfee82f4d
Ahmed Samir <39447006+ahmed-samir-mohammed@users.noreply.github.com>
2021-09-21 22:58:41 +0200

    changed

--- src/app/Pages/services/services.component.ts
+++ src/app/Pages/services/services.component.ts
@@ -32,6 +32,102 @@ export class ServicesComponent implements OnInit {
       category: 'خدمة 4',
       name: 'خدمة 4',
       imgName: 'img3.jpg'
+    },
+    {
+      id: 1,
+      category: 'خدمة 1',
+      name: 'خدمة 1',
+      imgName: 'img1.jpg'
+    },
+    {
+      id: 2,
+      category: 'خدمة 2',
+      name: 'خدمة 2', 
+      imgName: 'img3.jpg'
+    },
+    {
+      id: 3,
+      category: 'خدمة 3',
+      name: 'خدمة 3',
+      imgName: 'img1.jpg'
+    },
+    {
+      id: 4,
+      category: 'خدمة 4',
+      name: 'خدمة 4',
+      imgName: 'img3.jpg'
+    },
+    {
+      id: 1,
+      category: 'خدمة 1',
+      name: 'خدمة 1',
+      imgName: 'img1.jpg'
+    },
+    {
+      id: 2,
+      category: 'خدمة 2',
+      name: 'خدمة 2', 
+      imgName: 'img3.jpg'
+    },
+    {
+      id: 3,
+      category: 'خدمة 3',
+      name: 'خدمة 3',
+      imgName: 'img1.jpg'
+    },
+    {
+      id: 4,
+      category: 'خدمة 4',
+      name: 'خدمة 4',
+      imgName: 'img3.jpg'
+    },
+    {
+      id: 1,
+      category: 'خدمة 1',
+      name: 'خدمة 1',
+      imgName: 'img1.jpg'
+    },
+    {
+      id: 2,
+      category: 'خدمة 2',
+      name: 'خدمة 2', 
+      imgName: 'img3.jpg'
+    },
+    {
+      id: 3,
+      category: 'خدمة 3',
+      name: 'خدمة 3',
+      imgName: 'img1.jpg'
+    },
+    {
+      id: 4,
+      category: 'خدمة 4',
+      name: 'خدمة 4',
+      imgName: 'img3.jpg'
+    },
+    {
+      id: 1,
+      category: 'خدمة 1',
+      name: 'خدمة 1',
+      imgName: 'img1.jpg'
+    },
+    {
+      id: 2,
+      category: 'خدمة 2',
+      name: 'خدمة 2', 
+      imgName: 'img3.jpg'
+    },
+    {
+      id: 3,
+      category: 'خدمة 3',
+      name: 'خدمة 3',
+      imgName: 'img1.jpg'
+    },
+    {
+      id: 4,
+      category: 'خدمة 4',
+      name: 'خدمة 4',
+      imgName: 'img3.jpg'
     }
   ];
   
--- src/app/core/pages/news.ts
+++ src/app/core/pages/news.ts
@@ -1,9 +1,9 @@
 export interface NewsList {
-  id: number
-  title: string
-  date: string
-  imgName: string
-  disc_1: string
-  disc_2: string
-  disc_3: string
+  id: number,
+  title: string,
+  date: string,
+  imgName: string,
+  disc_1: string,
+  disc_2: string,
+  disc_3: string,
 }
