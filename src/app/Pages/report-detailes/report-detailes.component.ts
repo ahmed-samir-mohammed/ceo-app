60bb9e2df1d39036d63467b63694ade23056b040
Ahmed Samir <39447006+ahmed-samir-mohammed@users.noreply.github.com>
2021-11-17 23:35:36 +0200

    asdsdw

--- src/app/Pages/report-detailes/report-detailes.component.ts
+++ src/app/Pages/report-detailes/report-detailes.component.ts
@@ -5,29 +5,32 @@ import { ReportService } from 'src/app/core/services/report.service';
 @Component({
   selector: 'app-report-detailes',
   templateUrl: './report-detailes.component.html',
-  styleUrls: ['./report-detailes.component.scss']
+  styleUrls: ['./report-detailes.component.scss'],
 })
 export class ReportDetailesComponent implements OnInit {
-
   classes: string = 'bg-f4f6fc';
   reporItem: any;
   id!: any;
 
-  constructor(private reporListItem: ReportService, private route: ActivatedRoute) { }
+  constructor(
+    private reporListItem: ReportService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(id => {
+    this.route.paramMap.subscribe((id) => {
       this.id = id.get('id');
-      this.getCeoItem()
-    })
+      this.getCeoItem();
+    });
   }
   getCeoItem() {
-    this.reporListItem.getReportById(this.id).subscribe((res: any) => {
-      this.reporItem = res
-    },
-    err => {
-      console.log(err);
-    })
+    this.reporListItem.getReportById(this.id).subscribe(
+      (res: any) => {
+        this.reporItem = res;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
-
 }
